7706d634c9b663154d8fd4914fa544382672ba30
Shayan <aslani.shayan90@gmail.com>
2023-05-01 20:32:38 -0400

    Update init.sh

--- labfiles/parallel/init.sh
+++ labfiles/parallel/init.sh
@@ -13,5 +13,3 @@ $SUDO mv /tmp/bookapp.conf /etc/apache2/sites-available/
 $SUDO chown -R www-data:www-data /var/www/html/bookapp
 $SUDO a2dissite 000-default.conf
 $SUDO a2ensite bookapp.conf
-$SUDO systemctl start apache2
-$SUDO systemctl enable apache2
